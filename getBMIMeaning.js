b42bce8acb3b8f3f572272a661a3e6197b47bfe6
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-09-02 10:21:00 +0700

    finish BMI2

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,23 +2,24 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
-  let BMI;
-  BMI = weight / height*height;
-  return BMI;
+  return BMI = weight / (height*height);
+  
 }
 
 function getBMIMeaning(weight, height) {
+  //code here
   if (calculateBMI(weight,height)< 18.5) {
     return "Underweight";
   } else if (calculateBMI(weight,height)> 18.5 && calculateBMI(weight,height)<24.9) {
-    return "Normal Weight";
+    return "Normal weight";
   } else if(calculateBMI(weight,height) >25.0) {
     return "Overweight";
   }
 }
 
-console.log(getBMIMeaning(65, 1.8));
-console.log(getBMIMeaning(80, 1.7));
-console.log(getBMIMeaning(44, 1.6));
+// console.log(getBMIMeaning(65, 1.8));
+// console.log(getBMIMeaning(80, 1.7));
+// console.log(getBMIMeaning(44, 1.6));
 
 module.exports = getBMIMeaning
+ 
