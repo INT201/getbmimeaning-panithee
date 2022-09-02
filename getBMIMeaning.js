133a113b60240c362713686733287f0decd7cfb3
Panithee Virojwongchai <panithee.pnt@mail.kmutt.ac.th>
2022-09-02 08:45:52 +0700

    Finish getBMI

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,8 +2,23 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
+  let BMI;
+  BMI = weight / height*height;
+  return BMI;
 }
+
 function getBMIMeaning(weight, height) {
-  //code here
+  if (calculateBMI(weight,height)< 18.5) {
+    return "Underweight";
+  } else if (calculateBMI(weight,height)> 18.5 && calculateBMI(weight,height)<24.9) {
+    return "Normal Weight";
+  } else if(calculateBMI(weight,height) >25.0) {
+    return "Overweight";
+  }
 }
+
+console.log(getBMIMeaning(65, 1.8));
+console.log(getBMIMeaning(80, 1.7));
+console.log(getBMIMeaning(44, 1.6));
+
 module.exports = getBMIMeaning
